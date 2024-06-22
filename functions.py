f3ecd5a537ef2e1b98592bd784ca8a033ce7d4b2
Datt <dattpatel123@gmail.com>
2024-06-21 22:53:19 -0400

    bug fixes

--- functions.py
+++ functions.py
@@ -1,12 +1,9 @@
-import shutil
 import os
 import json
-import requests
-from dotenv import load_dotenv, dotenv_values   
+from dotenv import load_dotenv
 from collections import defaultdict
 import random
 import google.generativeai as genai
-from pprint import pprint
 
 #Load env variables
 load_dotenv()
@@ -23,15 +20,10 @@ def getDict():
     file_names = os.listdir('uploads') 
     random.shuffle(file_names)
 
-    print(f"Original Count: {len(file_names)}\n")
-
-
     # batch the uploaded files
     batch_size = 15  
 
     batches = [file_names[i:i + batch_size] for i in range(0, len(file_names), batch_size)]
-    
-    
     
     
     # Create empty dict and categories list
@@ -40,7 +32,7 @@ def getDict():
 
     # Call API to organize files in batches
 
-    for index, batch in enumerate(batches):
+    for batch in batches:
         
         tries = 0
 
@@ -57,7 +49,7 @@ def getDict():
                 
                 
                 categories.update(json.loads(response.text).keys())
-                print(f"\nBatch {index}'s Response: {response.text}\n\nCategories: {categories}\n")
+                #print(f"\nBatch {index}'s Response: {response.text}\n\nCategories: {categories}\n")
                 
                 # Add the organized batch into final dict
                 for key, value in json.loads(response.text).items():
@@ -72,13 +64,11 @@ def getDict():
             tries+=1
         
         
-    # Show me Final Dictionary and Categories
-    pprint(finalDict)
-    pprint(categories)
+    
 
     return finalDict
   
-
+# Organize file into categories
 def organize():
 
     finalDict = getDict()
@@ -86,23 +76,27 @@ def organize():
     # move files into folders
     for categoryName,files in finalDict.items():
 
-        #try: os.mkdir('uploads/'+categoryName)
-        #except:
-        #    shutil.rmtree('uploads/'+categoryName)
-        #    os.mkdir('uploads/'+categoryName)
+       
         os.mkdir('uploads/'+categoryName)
 
         for f in files:
             try: os.rename( 'uploads/' + f , 'uploads/' + categoryName + '/' + f)
             except Exception as e:
-                #os.remove('uploads/' + categoryName)
-               # os.rename( 'uploads/' + f , 'uploads/' + categoryName + '/' + f)
                
-               #print(f"------------")
-               print(f"\nError {e} w/ File: {f}\n")
+               print(f"\nError w/ file {f} ==> {e}\n")
                
-          
+
+# Give categories: count
+def findCounts():
+    totalInFiles = 0
+    for directory in os.listdir('uploads'):
+        if os.path.isdir('uploads/' + directory):
+            print(f"{directory}: {len(os.listdir('uploads/'+directory))}")
+            totalInFiles += len(os.listdir('uploads/'+directory))
+        else:
+            print('Extra file: ' + directory)
     
+    print(f"Total in Files:{totalInFiles}")
     
 
  
